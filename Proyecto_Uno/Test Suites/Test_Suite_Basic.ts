69872fe61e7a8bfe85cef79afec06a4e2c72728c
nglcode <nglcode@mailbox.org>
2023-02-07 21:15:59 +0100

    test suite for cmd run

---
+++ Proyecto_Uno/Test Suites/Test_Suite_Basic.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Suite_Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7e2b12ca-bcae-4cb8-a28d-df0b5a90d9cf</testSuiteGuid>
+   <testCaseLink>
+      <guid>a59936f0-e013-4a13-8a27-755cdaf3e21c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Prueba_1/Caso_1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>337285bf-9378-4e3b-9979-cca6bf9ece09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API_Test/Test_1_Get_Users</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
